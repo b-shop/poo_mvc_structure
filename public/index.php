e626c540c33e6c0ce56236095dc9764080012937
Amédée Bishop <ndarabumwoya@gmail.com>
2023-08-06 21:35:33 +0100

    third commit

--- public/index.php
+++ public/index.php
@@ -1,13 +1,12 @@
-<?php
-use App\Autoloader;
+<?php 
+
 use App\Core\Main;
 
 // On définit une constante contenant le dossier racine du projet
 define('ROOT', dirname(__DIR__));
 
 // On importe l'autoloader
-require_once ROOT.'/Autoloader.php';
-Autoloader::register();
+require_once ROOT.'vendor/autoload.php';
 
 // On instancie Main (notre routeur)
 $app = new Main();
